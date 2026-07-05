aea2248300d89b1db684067f06a3ae6760bd14ba
SamuelGuillot <samuel.guillot.pro@gmail.com>
2026-07-05 12:09:16 +0200

    Fixed rating during review creation

--- reviews/models.py
+++ reviews/models.py
@@ -3,9 +3,19 @@ from django.conf import settings
 from django.db import models
 
 
+RATING_CHOICES = [
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "3"),
+    (4, "4"),
+    (5, "5"),
+]
+
 class Review(models.Model):
     ticket = models.ForeignKey("tickets.Ticket", on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(5)],)
+    rating = models.IntegerField(choices=RATING_CHOICES,default=0,validators=[MinValueValidator(0),MaxValueValidator(5),]
+    )
     headline = models.CharField(max_length=128)
     body = models.CharField(max_length=8192, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
